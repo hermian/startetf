4b7d4f687be947d7686c5b7f6c4863c448c9f39e
hermian <hermian@gmail.com>
2022-01-26 18:43:01 +0900

    Update 2.38.py

--- 2.38.py
+++ 2.38.py
@@ -22,7 +22,9 @@ kospi
 
 
 # %% [markdown]
-# 월초리밸런싱을 가정하기 위해 월초로 된 데이터를 구성한다.
+# ## 월별 비중 데이터프레임 (WeighTarget)
+# 
+# 월말리밸런싱을 가정하기 위해 월말로 된 데이터를 구성한다.
 
 # %%
 
@@ -130,8 +132,8 @@ def momentumscore(data, weights, name="MomScore", start='1986-1-03'):
 
 # %%
 # shift(1)을 하면 전달 기준으로 모멘텀 계산
-bt_ms0 = momentumscore(data, weights0.dropna(), name='모멘텀 포트폴리오(KOSPI)')#.shift(1))
-bt_ms = momentumscore(data, weights.dropna(), name='모멘텀 포트폴리오(현금혼합)')#.shift(1))
+bt_ms0 = momentumscore(data, weights0, name='모멘텀 포트폴리오(KOSPI)')#.shift(1))
+bt_ms = momentumscore(data, weights, name='모멘텀 포트폴리오(현금혼합)')#.shift(1))
 
 
 # %%
@@ -139,13 +141,16 @@ bt_kospi = long_only_ew(data, ['KOSPI'], 'KOSPI')
 bt_현금 = long_only_ew(data, ['현금'], '현금')
 
 # %%
-r_ms = bt.run(bt_ms)#, bt_kospi, bt_ew)
+r_ms = bt.run(bt_ms)
 
 # %%
-start = '1986-1-31' # 최초 매수일
+start평균모멘텀스코어 = '1986-1-31' # 최초 매수일
 
 # %%
-r_ms.set_date_range(start)
+start수익곡선모멘텀 = '1986-7-31'
+
+# %%
+r_ms.set_date_range(start수익곡선모멘텀)
 r_ms.display()
 
 # %%
@@ -155,7 +160,7 @@ r_ms.plot(figsize=(12,8));
 r_all = bt.run(bt_ms, bt_ms0, bt_kospi, bt_ew)
 
 # %%
-r_all.set_date_range(start)
+r_all.set_date_range(start수익곡선모멘텀)
 r_all.display()
 
 # %%
@@ -167,12 +172,12 @@ r_kospi = bt.run(bt_kospi)
 r_ms0 = bt.run(bt_ms0)
 
 # %% [markdown]
-# # 그림 2.26
+# ### 그림 2.26
 
 # %%
-r_ms.set_date_range(start)
-r_kospi.set_date_range(start)
-r_현금.set_date_range(start)
+r_ms.set_date_range(start수익곡선모멘텀)
+r_kospi.set_date_range(start수익곡선모멘텀)
+r_현금.set_date_range(start수익곡선모멘텀)
 ax1 = r_ms.plot(color='g', figsize=(12,8));
 r_kospi.plot(ax=ax1, ls='--', color='b', figsize=(12, 8));
 r_현금.plot(ax=ax1, ls='--', color='r', figsize=(12, 8));
@@ -180,9 +185,10 @@ r_현금.plot(ax=ax1, ls='--', color='r', figsize=(12, 8));
 # r_ms0.plot(ax=ax1, ls='--', color='gray', figsize=(12, 8));
 
 # %% [markdown]
-# ## 수익곡선모멘텀(평균모멘텀스코어 6개월)
+# ### 수익곡선모멘텀(평균모멘텀스코어 6개월)
 
 # %%
+r_ms.set_date_range(start평균모멘텀스코어)
 r_ms.prices
 
 # %%
@@ -217,14 +223,11 @@ bt_ms1 = momentumscore(data1, weights1, name='수익곡선모멘텀전략')#.shi
 r_ms1 = bt.run(bt_ms1)
 
 # %%
-start수익곡선모멘텀 = '1986-7-31'
-
-# %%
 r_ms1.set_date_range(start수익곡선모멘텀)
 r_ms1.display()
 
 # %% [markdown]
-# ## 그림 2.38
+# ### 그림 2.38
 
 # %%
 r_ms.set_date_range(start수익곡선모멘텀)
@@ -242,10 +245,10 @@ r_ms1.plot(ax=ax1, ls='-', color='b', figsize=(12, 8));
 
 
 # %% [markdown]
-# # 일일데이터
+# ## 일일데이터 비중 클래스 구현 (일간->월간 변경 후 모멘텀스코어 구하는 법)
 
 # %%
-s, e
+s, e, start평균모멘텀스코어, start수익곡선모멘텀
 
 # %%
 # kospi = fdr.DataReader("KS11")['Close']
@@ -265,6 +268,9 @@ data_daily = pd.merge(data_daily, dd, left_index=True, right_index=True, how='le
 
 # %%
 data_daily
+
+# %% [markdown]
+# ### 평균모멘텀스코어 WeighAMSWithCash
 
 # %%
 # 월말에 계산하고 다음월 첫 거래일 종가로 거래 한다고 전재
@@ -346,11 +352,8 @@ bt_cash = momentum_mixedcash("모멘텀(현금혼합)", data, lags=0)
 r_cash = bt.run(bt_cash, bt_ms)
 
 # %%
-start
-
-# %%
-# 처음과 동일함을 검증(로직)
-r_cash.set_date_range(start)
+# 비중데이터프레임의 결과와 동일함을 검증(로직)
+r_cash.set_date_range(start수익곡선모멘텀)
 r_cash.display()
 
 # %%
@@ -360,17 +363,14 @@ bt_daily = momentum_mixedcash("모멘텀(현금혼합,일간)", data_daily, lags
 r_daily = bt.run(bt_daily)
 
 # %%
-s,e,start
+s,e,start평균모멘텀스코어,start수익곡선모멘텀
 
 # %%
-r_daily.set_date_range(start)
+r_daily.set_date_range(start수익곡선모멘텀)
 r_daily.display()
 
 # %%
-r_daily.prices[start:].resample('M').last().to_drawdown_series().describe()
-
-# %%
-s,e,start
+r_daily.prices[start수익곡선모멘텀:].resample('M').last().to_drawdown_series().describe()
 
 # %%
 bt_kospi1 = long_only_ew(data_daily, ['KOSPI'], 'KOSPI')
@@ -379,8 +379,8 @@ bt_현금1 = long_only_ew(data_daily, ['현금'], '현금')
 r_kospi1 = bt.run(bt_kospi1)
 r_현금1 = bt.run(bt_현금1)
 
-r_kospi1.set_date_range(start, e)
-r_현금1.set_date_range(start, e)
+r_kospi1.set_date_range(start수익곡선모멘텀, e)
+r_현금1.set_date_range(start수익곡선모멘텀, e)
 
 # %%
 ax1 = r_daily.plot(color='g', figsize=(12,8));
@@ -388,7 +388,11 @@ r_kospi1.plot(ax=ax1, ls='--', color='b', figsize=(12, 8));
 r_현금1.plot(ax=ax1, ls='--', color='r', figsize=(12, 8));
 
 # %%
+r_daily.set_date_range(start평균모멘텀스코어)
 r_daily.prices
+
+# %% [markdown]
+# ### 수익곡선모멘텀 WeighAMS
 
 # %%
 # 월말에 계산하고 다음월 첫 거래일 종가로 거래 한다고 전재
@@ -466,15 +470,12 @@ s6 = bt.Strategy('수익곡선모멘텀6',
 r_daily.prices
 
 # %%
-s,e,start
-
-# %%
 data_daily1 = pd.DataFrame()
 data_daily1['수익곡선모멘텀6'] = r_daily.prices
 # %%
 dd = pd.DataFrame()
-dd.index = pd.date_range(start, e, freq='D')
-denominator = len(pd.date_range(start, e, freq='D'))/len(pd.date_range(start, e, freq='Y'))
+dd.index = pd.date_range(start평균모멘텀스코어, e, freq='D')
+denominator = len(pd.date_range(start평균모멘텀스코어, e, freq='D'))/len(pd.date_range(start평균모멘텀스코어, e, freq='Y'))
 dd['현금'] = pow(1.03, 1/denominator)
 dd['현금'] = dd['현금'].shift(1, fill_value=1.0)
 dd['현금'] = dd['현금'].cumprod()
@@ -489,11 +490,11 @@ bt_s6 = bt.Backtest(s6, data_daily1, initial_capital=100000000)
 r_6 = bt.run(bt_s6)
 
 # %%
-start수익곡선모멘텀
-
-# %%
 r_6.set_date_range(start수익곡선모멘텀)
 r_6.display()
+
+# %% [markdown]
+# ### 그림 2.38 (일간)
 
 # %%
 r_daily.set_date_range(start수익곡선모멘텀)
@@ -504,3 +505,214 @@ r_kospi1.plot(ax=ax1, ls='--', color='g', figsize=(12, 8));
 r_6.plot(ax=ax1, ls='-', color='b', figsize=(12, 8));
 #----
 # r_ms0.plot(ax=ax1, ls='--', color='gray', figsize=(12, 8));
+
+# %% [markdown]
+# # bt 적 방법
+# 월말 리밸런싱(run_end_of_period=True) 계산은 하루전(lag=pd.DateOffset(days=1))
+
+# %% [markdown]
+# ### 평균모멘텀스코어 class WeighAverageMomentumScore(bt.Algo):
+# 
+
+# %%
+class WeighAverageMomentumScore(bt.Algo):
+    def __init__(self, months=12, lag=pd.DateOffset(days=0), cash='현금', cash_weigh=0.0, 현금자산제외=True):
+        super(WeighAverageMomentumScore, self).__init__()
+        self.lookback = months
+        self.lag = lag
+        self.cash = cash
+        self.cash_weigh = cash_weigh
+        self.현금자산제외 = 현금자산제외
+
+    def __call__(self, target):
+        selected = target.temp['selected'].copy()
+
+        # print("===", selected)
+        if self.현금자산제외: # 현금자산빼고 모멘텀 비중나누고 나머지는 현금으로 , 아니면 현금도 하나의 자산으로
+            selected.remove(self.cash)
+
+        t0 = target.now - self.lag
+
+        if target.universe[selected].index[0] > (t0 - pd.DateOffset(months=self.lookback)): # !!!
+            return False
+
+        momentums_score = 0
+        for lookback in range(1, self.lookback+1):
+            start = t0 - pd.DateOffset(months=lookback)
+            prc = target.universe[selected].loc[start:t0]
+            momentum_score = np.where(prc.calc_total_return() > 0, 1, 0)
+            momentums_score += momentum_score
+
+        average_momentum_score = momentums_score / self.lookback
+        print(average_momentum_score)
+
+        average_momentum_score *= (1 - self.cash_weigh)
+        
+        # XXX systrader79/backtesting/ebook/dynamic/10.mxied_korea_us.py 수정필요
+        if self.현금자산제외: ## XXX 이 로직이 맞는지 모르겠음
+            weights = average_momentum_score/len(selected) 
+            weights = pd.Series(weights, index=selected)
+            weights[self.cash] = 1-weights.sum()
+        else:
+            weights = average_momentum_score/average_momentum_score.sum()
+            weights = pd.Series(weights, index=selected)
+
+
+        target.temp['weights'] = weights
+
+        print(f"{target.now} ", end =" ")
+        for i, v in weights.items():
+            print(f"{i}:{v:.3f}", end=" ")
+        print("")
+        return True
+
+# %%
+#%%
+def average_momentum_score_mixed_cash(name, data, months=12, lag=pd.DateOffset(days=0), cash='현금', 현금자산제외=False):
+    st = bt.Strategy(name,
+        [
+            bt.algos.RunMonthly(run_on_first_date=True,
+                                run_on_end_of_period=True, #월말
+                                run_on_last_date=False),
+            # bt.algos.PrintInfo('{name} : {now}'),
+            bt.algos.SelectAll(),
+            WeighAverageMomentumScore(months=months, lag=lag, cash=cash, 현금자산제외=현금자산제외),
+            # bt.algos.PrintTempData(),
+            bt.algos.Rebalance()
+        ]
+    )
+    test = bt.Backtest(st, data, initial_capital=100000000)
+    return test
+
+# %%
+data_daily
+
+# %%
+bt_ams_mixed_cash = average_momentum_score_mixed_cash("평균모멘텀스코어(현금혼합)", data_daily, 12, pd.DateOffset(days=1), '현금')
+r_ams_mixed_cash = bt.run(bt_ams_mixed_cash)
+
+# %%
+r_ams_mixed_cash.set_date_range(start수익곡선모멘텀)
+r_ams_mixed_cash.display()
+
+# %%
+bt_kospi1 = long_only_ew(data_daily, ['KOSPI'], 'KOSPI')
+bt_현금1 = long_only_ew(data_daily, ['현금'], '현금')
+
+r_kospi1 = bt.run(bt_kospi1)
+r_현금1 = bt.run(bt_현금1)
+
+r_kospi1.set_date_range(start평균모멘텀스코어, e)
+r_현금1.set_date_range(start평균모멘텀스코어, e)
+
+# %%
+ax1 = r_ams_mixed_cash.plot(color='g', figsize=(12,8));
+r_kospi1.plot(ax=ax1, ls='--', color='b', figsize=(12, 8));
+r_현금1.plot(ax=ax1, ls='--', color='r', figsize=(12, 8));
+
+# %% [markdown]
+# ### 수익곡선모멘텀
+
+# %%
+r_ams_mixed_cash.set_date_range(start평균모멘텀스코어)
+r_ams_mixed_cash.prices
+
+# %%
+data_daily2 = pd.DataFrame()
+data_daily2['수익곡선모멘텀6'] = r_ams_mixed_cash.prices
+# %%
+dd = pd.DataFrame()
+dd.index = pd.date_range(start평균모멘텀스코어, e, freq='D')
+denominator = len(pd.date_range(start평균모멘텀스코어, e, freq='D'))/len(pd.date_range(start평균모멘텀스코어, e, freq='Y'))
+dd['현금'] = pow(1.03, 1/denominator)
+dd['현금'] = dd['현금'].shift(1, fill_value=1.0)
+dd['현금'] = dd['현금'].cumprod()
+#%%
+data_daily2 = pd.merge(data_daily2, dd, left_index=True, right_index=True, how='left')
+
+# %%
+data_daily2
+
+# %%
+bt_returns_curve_mom = average_momentum_score_mixed_cash("평균모멘텀스코어(현금혼합)+수익곡선모멘텀", 
+                                data_daily2, 6, pd.DateOffset(days=1), '현금', 현금자산제외=True)
+r_returns_curve_mom = bt.run(bt_returns_curve_mom)
+
+# %%
+r_returns_curve_mom.set_date_range(start수익곡선모멘텀)
+r_returns_curve_mom.display()
+
+# %%
+r_returns_curve_mom.get_security_weights().plot.area(figsize=(12,4));
+
+# %%
+r_returns_curve_mom.prices
+
+# %%
+plot_assets(r_returns_curve_mom, start수익곡선모멘텀, e, "평균모멘텀스코어(현금혼합)+수익곡선모멘텀")
+
+# %% [markdown]
+# ### 그림 2.38 (일간, bt적 알고)
+
+# %%
+r_ams_mixed_cash.set_date_range(start수익곡선모멘텀)
+r_kospi1.set_date_range(start수익곡선모멘텀)
+r_returns_curve_mom.set_date_range(start수익곡선모멘텀)
+ax1 = r_ams_mixed_cash.plot(color='r', figsize=(12,8));
+r_kospi1.plot(ax=ax1, ls='--', color='g', figsize=(12, 8));
+r_returns_curve_mom.plot(ax=ax1, ls='-', color='b', figsize=(12, 8));
+#----
+# r_ms0.plot(ax=ax1, ls='--', color='gray', figsize=(12, 8));
+
+# %% [markdown]
+# # 비교
+# 
+# - 월별 비중 데이터프레임
+#   - CAGR                 6.43%/Max Drawdown         -19.12%
+#   - CAGR                 6.44%/Max Drawdown         -10.19% (수익곡선모멘텀6)
+#   
+# - 일일데이터 비중 클래스
+#   - CAGR                 6.41%/Max Drawdown         -21.13%
+#   - CAGR                 6.39%/ Max Drawdown         -13.74% (수익곡선모멘텀6)
+# 
+# - bt적 방법
+#   - CAGR                 6.38%/Max Drawdown         -21.28%
+#   - CAGR                 6.30%/Max Drawdown         -12.88% (수익곡선모멘텀6)
+
+# %%
+r_daily.set_date_range(start수익곡선모멘텀)
+r_6.set_date_range(start수익곡선모멘텀)
+r_ams_mixed_cash.set_date_range(start수익곡선모멘텀)
+r_returns_curve_mom.set_date_range(start수익곡선모멘텀)
+#--
+ax1 = r_daily.plot(figsize=(12,8));
+# r_6.plot(ax=ax1,figsize=(12,8));
+#--
+r_ams_mixed_cash.plot(ax=ax1,figsize=(12,8));
+# r_returns_curve_mom.plot(ax=ax1,figsize=(12,8));
+
+# %%
+r_daily.set_date_range(start수익곡선모멘텀)
+r_6.set_date_range(start수익곡선모멘텀)
+r_ams_mixed_cash.set_date_range(start수익곡선모멘텀)
+r_returns_curve_mom.set_date_range(start수익곡선모멘텀)
+#--
+# ax1 = r_daily.plot(figsize=(12,8));
+ax1 = r_6.plot(figsize=(12,8));
+#--
+# r_ams_mixed_cash.plot(ax=ax1,figsize=(12,8));
+r_returns_curve_mom.plot(ax=ax1,figsize=(12,8));
+
+# %%
+r_daily.set_date_range(start수익곡선모멘텀)
+r_6.set_date_range(start수익곡선모멘텀)
+r_ams_mixed_cash.set_date_range(start수익곡선모멘텀)
+r_returns_curve_mom.set_date_range(start수익곡선모멘텀)
+#--
+ax1 = r_daily.plot(figsize=(12,8));
+r_6.plot(ax=ax1,figsize=(12,8));
+#--
+r_ams_mixed_cash.plot(ax=ax1,figsize=(12,8));
+r_returns_curve_mom.plot(ax=ax1,figsize=(12,8));
+
+# %%
